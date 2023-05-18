2bb3869de2f158d7bd5aef7c05820ef966c4e807
DA M1 59 <selinankunas@gmail.com>
2023-05-18 16:04:15 +0200

    added main

--- src/data/user.js
+++ src/data/user.js
@@ -27,37 +27,37 @@ const INFO = {
 		{
 			title: "HTML",
 			logo: "https://img.icons8.com/color/48/html-5--v1.png",
-			description:"hgfuykiugilugugkuygukygku",
+			description:"The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
 		},
 
 		{
 			title: "CSS",
 			logo: "https://img.icons8.com/color/48/css3.png",
-			description:"hgfuykiugilugugkuygukygku",
+			description:"hCascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.",
 		},
 
 		{
 			title: "JavaScript",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			description:"hgfuykiugilugugkuygukygku",
+			description:"JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
 		},
 
 		{
 			title: "React.js",
 			logo: "https://img.icons8.com/color/48/react-native.png",
-			description:"hgfuykiugilugugkuygukygku",
+			description:"React is a free and open-source front-end JavaScript library for building user interfaces based on components.",
 		},
 
 		{
 			title: "Discord",
 			logo: "https://img.icons8.com/color/48/discord--v2.png",
-			description:"hgfuykiugilugugkuygukygku",
+			description:"Discord is a VoIP and instant messaging social platform. Users have the ability to communicate with voice calls, video calls, text messaging,",
 		},
 
 		{
 			title: "Bootstrap 5",
 			logo: "https://img.icons8.com/color/48/bootstrap.png",
-			description:"hgfuykiugilugugkuygukygku",
+			description:"Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development.",
 		},
 	],
 };
